64ea6deb5b877386a97474c56c8e1b47422c6186
isa0801 <isa080199@gmail.com>
2021-06-10 00:46:06 +0200

    adding style change

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -12,17 +12,41 @@ const Container = (props) => {
         setToggle(!toggle)
     }
 
+    const showList = () => {
+        return (
+                listArr.map((item, idx) =>
+                    <h3 style={{color: "white", display: "flex"}} key={idx}>{item}</h3>
+                )
+        )
+    }
+
     const toggleStyle = {
         transform: "rotate(90deg)",
         padding:"2px 0px 0px 4px",
 
     }
+
+    const listArr = [
+        "Learn Math",
+        "Learn PHP",
+        "Learn ASM"
+    ]
     
 
   return (
+      <div style={{margin: "auto"}}>
         <div class="container" onClick={handleClick}>
             <IoIosArrowForward style={toggle? toggleStyle:null} />
-            <h2 style={{color: "white", cursor: "pointer"}} >{props.title? props.title:"Undefined"}</h2>
+            
+            
+                <h2 className="listTtile" style={{color: "white", cursor: "pointer"}} >{props.title? props.title:"Undefined"}</h2>
+
+        </div>
+        <div className="list">
+        { 
+                    toggle? showList():null
+        }
+                    </div>
         </div>
   );
 };
